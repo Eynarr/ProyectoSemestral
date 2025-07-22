d75554468544be0dd457425fc0e376a364e8e548
rnanm <renan.marquez@utp.ac.pa>
2025-07-21 20:16:42 -0500

    Modificación 2

--- app/src/main/java/com/example/proyectosemestral/proximity.java
+++ app/src/main/java/com/example/proyectosemestral/proximity.java
@@ -40,7 +40,7 @@ public class proximity extends AppCompatActivity {
                         estado.setText("¡Algo está cerca!");
                     } else {
                         mainLayout.setBackgroundColor(Color.GREEN);
-                        estado.setText("WOOOOOOOOOOOOOOOW Nada cerca");
+                        estado.setText("Nada cerca");
                     }
                 }
 
