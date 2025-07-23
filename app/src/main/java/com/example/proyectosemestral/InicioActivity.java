ff892c7396ee5c98111a3302a4bfded96baf8d72
rnanm <renan.marquez@utp.ac.pa>
2025-07-22 22:08:14 -0500

    Modificación Titulo de juego e imagen en inicio

--- app/src/main/java/com/example/proyectosemestral/InicioActivity.java
+++ app/src/main/java/com/example/proyectosemestral/InicioActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InicioActivity extends AppCompatActivity {
 
     private Button btnJuego, btnSensor;
+    private ImageView imagenCarita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        imagenCarita = findViewById(R.id.imagenCarita);
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.girar);
+        imagenCarita.startAnimation(animacion);
 
         btnJuego = findViewById(R.id.btnIrJuego);
         btnSensor = findViewById(R.id.btnIrSensor);
